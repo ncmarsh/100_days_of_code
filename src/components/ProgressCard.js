2da496d37b3b0df2c905ab9527100ac3b8505ed8
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-03-08 20:27:14 -0800

    updated today's progress

--- src/components/ProgressCard.js
+++ src/components/ProgressCard.js
@@ -23,7 +23,7 @@ function ProgressCard() {
             <Card.Text>
                 Remember you're not alone! It helped me to read some articles from others who said the hardest part is getting started.
             </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="https://github.com/ncmarsh/100_days_of_code/commit/c18783e690487a3a79e9e5301d8f8388419e6ed5">One of today's commits</Card.Link>
         </Card.Body>
         </Card>
     )
--- src/components/ProgressTable.js
+++ src/components/ProgressTable.js
@@ -23,7 +23,11 @@ function ProgressTable() {
                     <td>I started!</td>
                     <td>Deciding what to work on</td>
                     <td>Remember you're not alone! It helped me to read some articles from others who said the hardest part is getting started.</td>
-                    <td></td>
+                    <td>
+                        <a href="https://github.com/ncmarsh/100_days_of_code/commit/c18783e690487a3a79e9e5301d8f8388419e6ed5">
+                            One of today's commits
+                        </a>
+                    </td>
                 </tr>
                 {/* <tr>
                     <td>2</td>
