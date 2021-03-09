b76f5d7bd62c9670b97e7b005ec191345dce8509
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-03-08 20:19:27 -0800

    created simple navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+function NavBar() {
+    return (
+        <Navbar expand="lg" variant="light" bg="light">
+            <Navbar.Brand href="#">100 Days Of Code</Navbar.Brand>
+        </Navbar>
+    )
+}
+
+export default NavBar;
